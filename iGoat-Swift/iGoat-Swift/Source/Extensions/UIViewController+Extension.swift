6374f632ba8d900aa8d1d67aeaef3a01b50a4502
ZeroPath <noreply@zeropath.com>
2025-10-20 06:25:53 +0000

    fix: prevent crash on alert presentation with nil root VC

--- iGoat-Swift/iGoat-Swift/Source/Extensions/UIViewController+Extension.swift
+++ iGoat-Swift/iGoat-Swift/Source/Extensions/UIViewController+Extension.swift
@@ -5,25 +5,38 @@ extension UIViewController {
         return T(nibName: String(describing: self), bundle: nil)
     }
     
+    private class func keyRootViewController() -> UIViewController? {
+        if #available(iOS 13.0, *) {
+            return UIApplication.shared.connectedScenes
+                .compactMap { $0 as? UIWindowScene }
+                .flatMap { $0.windows }
+                .first(where: { $0.isKeyWindow })?.rootViewController
+        } else {
+            return UIApplication.shared.keyWindow?.rootViewController ?? UIApplication.shared.windows.first?.rootViewController
+        }
+    }
+
     class func topVisibleViewController(of viewController: UIViewController? = nil) -> UIViewController {
         var viewController = viewController
         if viewController == nil {
-            viewController = UIApplication.shared.keyWindow?.rootViewController
+            viewController = keyRootViewController() ?? UIApplication.shared.windows.first?.rootViewController
         }
-        
+
         if let navigationController = viewController as? UINavigationController,
             navigationController.viewControllers.count != 0 {
-            return topVisibleViewController(of:navigationController.viewControllers.last)
+            return topVisibleViewController(of: navigationController.viewControllers.last)
         }
-            
+
         else if let tabBarController = viewController as? UITabBarController,
             let selectedViewController = tabBarController.selectedViewController {
             return topVisibleViewController(of: selectedViewController)
         }
-            
+
         else if let presentedController = viewController?.presentedViewController {
             return topVisibleViewController(of: presentedController)
         }
-        return viewController!
+
+        // As a last resort, return a new UIViewController to avoid force-unwrapping nil
+        return viewController ?? UIViewController()
     }
 }

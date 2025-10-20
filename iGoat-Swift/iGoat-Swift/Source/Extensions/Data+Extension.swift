446442a0ed663a3322a8f75a45088dd43dc111f7
ZeroPath <noreply@zeropath.com>
2025-10-20 06:00:31 +0000

    fix: use random key and IV for AES encryption

--- iGoat-Swift/iGoat-Swift/Source/Extensions/Data+Extension.swift
+++ iGoat-Swift/iGoat-Swift/Source/Extensions/Data+Extension.swift
@@ -1,41 +1,80 @@
 import Foundation
+import CommonCrypto
+import Security
 
 extension Data{
-    func aes(operation: Int = kCCEncrypt, keyData: Data,
-                     ivData: Data = "abcdefghijklmnop".data(using:String.Encoding.utf8)!
-                     ) -> Data {
-        let dataLength = self.count
+    func aes(operation: Int = kCCEncrypt, keyData: Data, ivData: Data? = nil) -> Data {
+        let blockSize = kCCBlockSizeAES128
+        let iv: Data
+        var inputData: Data
+
+        if operation == kCCEncrypt {
+            if let providedIv = ivData {
+                iv = providedIv
+            } else {
+                var ivBytes = Data(count: blockSize)
+                let result = ivBytes.withUnsafeMutableBytes { ptr -> Int32 in
+                    guard let base = ptr.baseAddress else { return -1 }
+                    return SecRandomCopyBytes(kSecRandomDefault, blockSize, base)
+                }
+                if result != errSecSuccess {
+                    iv = Data(repeating: 0, count: blockSize)
+                } else {
+                    iv = ivBytes
+                }
+            }
+            inputData = self
+        } else {
+            // decrypt: if no IV provided, expect it to be prepended to the ciphertext
+            if let providedIv = ivData {
+                iv = providedIv
+                inputData = self
+            } else {
+                guard self.count > blockSize else { return Data() }
+                iv = self.subdata(in: 0..<blockSize)
+                inputData = self.subdata(in: blockSize..<self.count)
+            }
+        }
+
+        let dataLength = inputData.count
         let cryptLength  = size_t(dataLength + kCCBlockSizeAES128)
         var cryptData = Data(count:cryptLength)
-        
-        let keyLength = size_t(kCCKeySizeAES128)
+
+        let keyLength = size_t(keyData.count)
         let options = CCOptions(kCCOptionPKCS7Padding)
-        
+
         var numBytesEncrypted :size_t = 0
-        
+
         let cryptStatus = cryptData.withUnsafeMutableBytes {cryptBytes in
-            self.withUnsafeBytes {dataBytes in
-                ivData.withUnsafeBytes {ivBytes in
+            inputData.withUnsafeBytes {dataBytes in
+                iv.withUnsafeBytes {ivBytes in
                     keyData.withUnsafeBytes {keyBytes in
                         CCCrypt(CCOperation(operation),
                                 CCAlgorithm(kCCAlgorithmAES),
                                 options,
-                                keyBytes, keyLength,
-                                ivBytes,
-                                dataBytes, dataLength,
-                                cryptBytes, cryptLength,
+                                keyBytes.baseAddress, keyLength,
+                                ivBytes.baseAddress,
+                                dataBytes.baseAddress, dataLength,
+                                cryptBytes.baseAddress, cryptLength,
                                 &numBytesEncrypted)
                     }
                 }
             }
         }
-        
+
         if UInt32(cryptStatus) == UInt32(kCCSuccess) {
             cryptData.removeSubrange(numBytesEncrypted..<cryptData.count)
-            
+            if operation == kCCEncrypt {
+                // prepend IV to ciphertext so it can be used for decryption
+                var output = Data()
+                output.append(iv)
+                output.append(cryptData)
+                return output
+            }
         } else {
             print("Error: \(cryptStatus)")
+            return Data()
         }
-        return cryptData;
+        return cryptData
     }
 }
